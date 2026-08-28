e125197f058ce8693c76471ee1ea090b9c5b8e70
Martina_Leggiero <martina.leggiero@gmail.com>
2026-08-28 10:25:12 +0200

    comments

--- src/js/sign.js
+++ src/js/sign.js
@@ -12,35 +12,35 @@ export function createSign(scene) {
     const ctx = canvas.getContext("2d");
 
    // Sfondo
-ctx.fillStyle = "#E8DCC0";
-ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-// Bordo
-ctx.strokeStyle = "#5b4636";
-ctx.lineWidth = 8;
-ctx.strokeRect(10, 10, canvas.width - 20, canvas.height - 20);
-
-// Testo
-ctx.fillStyle = "#222222";
-ctx.textAlign = "center";
-
-// Icona
-ctx.font = "90px serif";
-ctx.fillText("🏺", canvas.width / 2, 120);
-
-// Titolo
-ctx.font = "bold 64px Cinzel";
-ctx.fillText("NECROPOLIS OF TARQUINIA", canvas.width / 2, 210);
-
-// Linea
-ctx.beginPath();
-ctx.moveTo(150, 260);
-ctx.lineTo(874, 260);
-ctx.stroke();
-
-// Welcome
-ctx.font = "bold 68px Cinzel";
-ctx.fillText("Welcome", canvas.width / 2, 380);
+    ctx.fillStyle = "#E8DCC0";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    
+    // Bordo
+    ctx.strokeStyle = "#5b4636";
+    ctx.lineWidth = 8;
+    ctx.strokeRect(10, 10, canvas.width - 20, canvas.height - 20);
+    
+    // Testo
+    ctx.fillStyle = "#222222";
+    ctx.textAlign = "center";
+    
+    // Icona
+    ctx.font = "90px serif";
+    ctx.fillText("🏺", canvas.width / 2, 120);
+    
+    // Titolo
+    ctx.font = "bold 64px Cinzel";
+    ctx.fillText("NECROPOLIS OF TARQUINIA", canvas.width / 2, 210);
+    
+    // Linea
+    ctx.beginPath();
+    ctx.moveTo(150, 260);
+    ctx.lineTo(874, 260);
+    ctx.stroke();
+    
+    // Welcome
+    ctx.font = "bold 68px Cinzel";
+    ctx.fillText("Welcome", canvas.width / 2, 380);
     // Texture
     const signTexture = new THREE.CanvasTexture(canvas);
 
